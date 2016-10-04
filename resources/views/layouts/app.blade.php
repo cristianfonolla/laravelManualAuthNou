509e7115a1c70ff74e0f4c80cb3cf743255496af
cristianfonolla <cristian.fonolla@gmail.com>
2016-10-04 20:51:12 +0200

    layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+
+@include('layouts.partials.htmlheader')
+
+
+<body>
+
+@include('layouts.partials.sidebar')
+
+@yield('main-content')
+
+@include('layouts.partials.footer')
+
+</body>
+</html>
