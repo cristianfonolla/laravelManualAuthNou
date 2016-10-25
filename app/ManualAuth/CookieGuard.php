c6b8cf521efc9a8980e2d1fb9b463b187568f778
cristianfonolla <cristian.fonolla@gmail.com>
2016-10-25 21:50:51 +0200

    delegaction done

--- app/ManualAuth/CookieGuard.php
+++ app/ManualAuth/CookieGuard.php
@@ -9,17 +9,30 @@ namespace App\ManualAuth;
  */
 class CookieGuard implements Guard
 {
+    protected $provider;
 
     /**
-     * ManualGuard constructor.
+     * CookieGuard constructor.
+     * @param $provider
      */
-    public function __construct()
+    public function __construct(UserProvider $provider)
     {
-
+        $this->provider = $provider;
     }
+
 
     public function check()
     {
         return isset($_COOKIE['user']) ? true : false;
+    }
+
+    public function validate(array $credentials)
+    {
+       return $this->provider->validate($credentials);
+    }
+
+    public function setUser($user)
+    {
+        // TODO: Implement setUser() method.
     }
 }
