18d43a8aa8890078848bd55d6d8cfe6f6fbf4b3a
cristianfonolla <cristian.fonolla@gmail.com>
2017-05-10 11:48:12 +0200

    sync

--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::group(['middleware' => 'manualauth'], function () {
     Route::get('/tasques', function () {
         return view('tasques');
     });
+    
     Route::get('/home', function () {
         return view('home');
     });
